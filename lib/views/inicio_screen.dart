2c4351870cefeacae7341500dc6e22f5d6d3b19b
virden18 <virgilio.denis18@gmail.com>
2025-04-03 19:26:41 -0300

    Añadir estado con StatefulWidget
    Se actualizó a StatefulWidget el 'InicioScreen' anetiormente StatelessWidget,
    permitiendo agregar un contador que incrementa cada vez que se presiona un
    botón.

--- lib/views/inicio_screen.dart
+++ lib/views/inicio_screen.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:vdenis_flutter_002/views/acercade_screen.dart';
 import 'package:vdenis_flutter_002/views/contacto_screen.dart';
 
-class InicioScreen extends StatelessWidget {
+class InicioScreen extends StatefulWidget {
   const InicioScreen({super.key});
+
+  @override
+  _InicioScreenState createState() => _InicioScreenState();
+}
+
+class _InicioScreenState extends State<InicioScreen> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Aquí puedes inicializar cualquier cosa que necesites al cargar la pantalla
+    print('initState: La pantalla Inicio ha sido inicializada.');
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +35,12 @@ class InicioScreen extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
+            Text(
+              'Haz presionado el botón $_counter veces',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -38,6 +62,16 @@ class InicioScreen extends StatelessWidget {
             )
           ]
         ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          )
+        ]
       ),
     );
   }
